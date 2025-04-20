3c1fbe64ec5b222d2487586d5d740e0cc9a743e2
Tomy2039 <tramosyahari@gmail.com>
2025-04-20 18:40:32 -0300

    cambie el fondo de la imagen por un video y cambie el diseño de la barra de navegacion por uno por color transparente y tambien agregue un mejor hover para la definicion de cyberseguridad

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,19 @@ const App = () => {
       <section
         id="inicio"
         className="h-screen bg-cover bg-center relative flex items-center text-violet-950 justify-start px-12"
-        style={{ backgroundImage: 'url("/cyber-security-and-information-or-network-protection-future-technology-background-free-vector.jpg")' }}
       >
+        <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0">
+          <source src="/video/5223113-hd_3840_2160_30fps.mp4" type="video/mp4"/>
+          Tu navegador no soporta video HTML5.
+        </video>
         <div className="relative text-white max-w-xl z-10">
-          <h1 className="text-5xl sm:text-6xl font-extrabold text-amber-300 mb-4 drop-shadow-lg flex items-center gap-4">
+          <h1 className="text-5xl sm:text-6xl font-extrabold text-amber-300 mb-4 drop-shadow-lg flex items-center gap-4 transition-all duration-500 transform hover:translate-x-3 hover:text-amber-400">
             <FontAwesomeIcon icon={faShieldHalved}/>Ciberseguridad</h1>
-          <p className="text-lg">
-          Ciberseguridad es la práctica de proteger sistemas informáticos, redes, programas y datos contra accesos no autorizados, ataques, daños o robos. Su objetivo es garantizar la confidencialidad, integridad y disponibilidad de la información digital.
-          </p>
+            <p className="relative text-lg transition-all duration-500 hover:scale-105 hover:bg-black/30 backdrop-blur-sm hover:text-white px-4 py-3 rounded-2xl border border-white/10 shadow-inner overflow-hidden group">
+              <span className="relative z-10">Ciberseguridad es la práctica de proteger sistemas informáticos, redes, programas y datos contra accesos no autorizados, ataques, daños o robos. Su objetivo es garantizar la confidencialidad, integridad y disponibilidad de la información digital.</span>
+              <span className="absolute bottom-0 left-0 w-0 h-1 bg-amber-400 transition-all duration-500 group-hover:w-full"></span>
+            </p>
+
         </div>
       </section>
       <section
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="fixed top-0 w-full bg-violet-950 text-white shadow-md shadow-violet-950 z-50">
+    <nav className="fixed top-0 w-full bg-violet-950/75 border border-white/10 backdrop-blur-md text-white shadow-md shadow-violet-950 z-50 rounded-b-3xl transition-all duration-500">
       <div className="flex items-center justify-between px-4 py-4">
         <div className="flex items-center space-x-2 group cursor-pointer">
           <img src="../../public/cyber_security.png" alt="Logo" className="h-10 transition-transform duration-500 group-hover:rotate-6 group-hover:scale-110 group-hover:animate-pulse" />
@@ -34,7 +34,7 @@ const Nav = () => {
             <li key={item.id}>
               <button
                 onClick={() => handleScroll(item.id)}
-                className="group flex items-center overflow-hidden bg-transparent hover:bg-white hover:text-violet-950 transition-all duration-300 px-2 py-2 rounded-lg w-10 hover:w-45 font-bold"
+                className="group flex items-center overflow-hidden bg-transparent hover:bg-white hover:text-violet-950 transition-all duration-300 px-2 py-2 rounded-full w-10 hover:w-45 font-bold"
               >
                 <FontAwesomeIcon icon={item.icon} className="text-xl" />
                 <span className="ml-3 whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300">
