aec8902c897dcba0993809116e29d690236d1167
Cristian D. Gonzalez <cristiand.gonzalez094@gmail.com>
2025-04-20 17:10:43 -0300

    feat: :art: Mejoras de diseño

--- src/components/Anual.jsx
+++ src/components/Anual.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Anual = () => {
 
     return (
-
         <img src="/01_pais_por_año.png" alt="pais_por_anio" />
 
     );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,66 +1,54 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faShieldHalved,
+  faMapLocationDot,
+  faChartLine,
+  faBug
+} from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
+  const handleScroll = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-    const handleScroll = (id) => {
-
-        const section = document.getElementById(id);
-
-        if (section) {
-            section.scrollIntoView({ behavior: "smooth" });
-        };
-
-    };
-
-    return (
-
-        <nav className="fixed top-0 w-full bg-violet-950 text-white shadow-md shadow-violet-950 z-50">
-            <ul className="flex justify-around py-4">
-                <li>
-                    <button
-                        onClick={() => handleScroll("inicio")}
-                        className="text-lg px-4 py-2 hover:text-amber-300"
-                    >
-                        Inicio
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={() => handleScroll("anual")}
-                        className="text-lg px-4 py-2 hover:text-amber-300"
-                    >
-                        Ataques Anuales
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={() => handleScroll("mapa")}
-                        className="text-lg px-4 py-2 hover:text-amber-300"
-                    >
-                        Mapa
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={() => handleScroll("evolucion")}
-                        className="text-lg px-4 py-2 hover:text-amber-300"
-                    >
-                        Evolución
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={() => handleScroll("tipos")}
-                        className="text-lg px-4 py-2 hover:text-amber-300"
-                    >
-                        Tipos de Ataques
-                    </button>
-                </li>
-            </ul>
-        </nav>
-
-    );
-
+  return (
+    <nav className="fixed top-0 w-full bg-violet-950 text-white shadow-md shadow-violet-950 z-50">
+      <div className="flex items-center justify-between px-4 py-4">
+        <div
+          onClick={() => handleScroll("inicio")}
+          className="flex items-center space-x-2 group cursor-pointer">
+          <img src="/cyber_security.png" alt="Logo" className="h-10 transition-transform duration-500 group-hover:rotate-6 group-hover:scale-110" />
+          <span className="text-2xl font-bold tracking-wide transition-all duration-500 transform group-hover:translate-x-1 group-hover:text-amber-300">Cloud Cybersecurity</span>
+        </div>
+        <ul className="flex space-x-4">
+          {[
+            { id: "inicio", icon: faHouse, label: "Inicio" },
+            { id: "anual", icon: faShieldHalved, label: "Ataques Anuales" },
+            { id: "mapa", icon: faMapLocationDot, label: "Mapa" },
+            { id: "evolucion", icon: faChartLine, label: "Evolución" },
+            { id: "tipos", icon: faBug, label: "Tipos de Ataques" },
+          ].map((item) => (
+            <li key={item.id}>
+              <button
+                onClick={() => handleScroll(item.id)}
+                className="group flex items-center overflow-hidden bg-transparent hover:bg-white hover:text-violet-950 transition-all duration-300 px-2 py-2 rounded-lg w-10 hover:w-45 font-bold"
+              >
+                <FontAwesomeIcon icon={item.icon} className="text-xl" />
+                <span className="ml-3 whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  {item.label}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
